90d3808f7a7fb66df8321b4a4aaa807a7e471375
William <william.jsson@hotmail.com>
2022-09-15 18:27:31 +0200

    fix

---
+++ spel/visuals.py
@@ -0,0 +1,12 @@
+import time
+
+import game
+
+def main_visual():
+    while True:
+        time.sleep(1)
+        print("IT WORKED WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+
+
+if __name__ == '__main__':
+    main_visual()
