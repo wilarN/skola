3c06951496f73f0d3cd3a665ef8979a58ad7c704
william <wille.vincent@hotmail.com>
2023-02-01 13:24:24 +0100

    l

--- Web/JS/JsTestWebsite/script.js
+++ Web/JS/JsTestWebsite/script.js
@@ -5,7 +5,8 @@
 let password = "awesomepassword";
 let attempts = 0;
 
-passwordPrompt();
+
+document.getElementById('profPic').style.boxShadow = "2px 2px 30px cyan";
 
 function passwordPrompt(){
     attempts +=1;
@@ -20,4 +21,28 @@ function passwordPrompt(){
         }
     }
     document.getElementById('attempts').innerHTML = attempts;
+}
+
+function passwordCheck(){
+    attempts +=1;
+    let usr_try_pass = document.getElementById('passInput').value;
+    console.log(usr_try_pass);
+    if (document.getElementById("status").style.display === "none" || document.getElementById("att").style.display === "none"){
+        document.getElementById("status").style.display = "block";
+        document.getElementById("att").style.display = "block";  
+    }
+    if(usr_try_pass == password){
+        document.getElementById('result').innerHTML = "Logged in.";
+        document.getElementById('profPic').style.boxShadow = "2px 2px 30px green";
+        document.getElementById('profPic').style.height = "3000px";
+        document.getElementById('bigpic').style.display='block';
+    } else{
+        document.getElementById('result').innerHTML = "Wrong password";
+        document.getElementById('profPic').style.boxShadow = "2px 2px 30px red";
+        document.getElementById('profPic').style.height = "100px";
+    }
+
+    document.getElementById('attempts').innerHTML = attempts;
+
+
 }
