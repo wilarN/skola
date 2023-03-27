0dad59a75549f1b94349c7c5a428c8547b115911
william <wille.vincent@hotmail.com>
2023-03-27 10:07:58 +0200

    d

---
+++ Web/JS/uppg/script.js
@@ -0,0 +1,11 @@
+
+let global_num = 10;
+
+function multByTwo(num){
+    return (num * 2);
+}
+
+function loaded(){
+    document.writeln(block_variable["Name"]);
+    document.writeln(block_variable["Age"]);
+}
