075aa2557cd1dc476f78ca699b7668f81d2bd685
william <wille.vincent@hotmail.com>
2023-10-18 09:38:26 +0200

    Update

--- Web/JS/array/script.js
+++ Web/JS/array/script.js
@@ -1,9 +1,15 @@
 
 
 function main_func(){
+/*
+const fav_food = ["Pizza", "Pasta", "Tortellini", "Sushi"];
+console.log(fav_food);
+
 const countries = ["Sweden", "Denmark", "Finland", "France"];
 console.log(countries);
+ */
 
+/*
 const Colours = ["Green", "Yellow", "Red", "Blue", "Purple"];
 console.log(Colours);
 
@@ -12,17 +18,17 @@ console.log(Colours);
 
 Colours.splice(2, 1);
 console.log(Colours);
-
+*/
 
 const Cities = ["Göteborg", "Stockholm", "Malmö", "Uppsala", "Jönköping"];
 
 Cities.unshift("Upplandsväsby");
 console.log(Cities);
 
-Cities.splice(4, 1);
+Cities.splice(-1, 1);
 console.log(Cities);
 
-Cities.splice(5, 0, "Örebro");
+Cities.splice(-1, 0, "Örebro");
 console.log(Cities);
 
 Cities.unshift("Venedig");
