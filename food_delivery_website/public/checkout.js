363c01a1fd4b507a4199147fe67eeb5493482bc9
glubb onobb <glubo@5CD132GMJ8.learnet.se>
2024-01-12 11:47:44 +0100

    Update

--- food_delivery_website/public/checkout.js
+++ food_delivery_website/public/checkout.js
@@ -1,14 +1,36 @@
+// Import all menu items from order.js
+
+import { menuItems } from './menu.js';
+
 
 function getOrderFromGetParams(){
     let getParams = window.location.search;
     let getParamsArray = getParams.split("&");
-    let orderArray = [];
-    if(orderArray.length > 0){
+    if(getParamsArray.length > 0){
         // Remove first ? from element at index 0.
         getParamsArray[0] = getParamsArray[0].slice(1);
     }
 
-    console.log(getParamsArray);
+    let orderArray = [];
+    getParamsArray.forEach(item => {
+        orderArray.push(menuItems.find(menuItem => menuItem.id === parseInt(item, 10)));
+    });
+    return orderArray;
 }
 
-getOrderFromGetParams();
+function displayOrder(){
+    const my_order = getOrderFromGetParams();
+    const orderContainer = document.querySelector("#order-container");
+
+    if(my_order.length > 0){
+        const ul = document.createElement('ul');
+        my_order.forEach(item => {
+            const li = document.createElement('li');
+            const div = document.createElement('div');
+        });
+    } else {
+        orderContainer.textContent = 'Your order is empty.';
+    }
+}
+
+displayOrder();
---
+++ food_delivery_website/public/menu.js
@@ -0,0 +1,24 @@
+// All available menu items
+export const menuItems = [
+    { id: 1, name: "Sashimi Mikado", price: 175 },
+    { id: 2, name: "Sashimi Shake", price: 175 },
+    { id: 3, name: "Tempura Moriwase", price: 175 },
+    { id: 4, name: "Syo Zin Age", price: 105 },
+    { id: 5, name: "Agedashi-Tofu", price: 105 },
+    { id: 6, name: "Ise-Ebi", price: 295 },
+    { id: 7, name: "Hotategai Furai", price: 190 },
+    { id: 8, name: "Taisho-Ebi", price: 175 },
+    { id: 9, name: "Yakitori", price: 120 },
+    { id: 10, name: "Harumaki", price: 105 },
+    { id: 11, name: "8 sorterade bitar", price: 160 },
+    { id: 12, name: "10 sorterade bitar", price: 180 },
+    { id: 13, name: "extra valfri bit", price: 17 },
+    { id: 14, name: "Tempura Maki", price: 215 },
+    { id: 15, name: "Fusion Makirulle", price: 195 },
+    { id: 16, name: "Dragon Makirulle", price: 195 },
+    { id: 17, name: "Rainbow Makirulle", price: 195 },
+    { id: 18, name: "Shio Yaki Makirulle", price: 195 },
+    { id: 19, name: "Yakiniku Makirulle", price: 215 },
+    { id: 20, name: "California Makirulle", price: 160 },
+    { id: 21, name: "Purjolök Makirulle", price: 160 }
+];
