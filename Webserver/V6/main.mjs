ee7ecd2832a63494929783eef04bbeeb217f9d23
William <william.jsson@hotmail.com>
2024-02-07 13:07:19 +0100

    Comments

--- Webserver/V6/main.mjs
+++ Webserver/V6/main.mjs
@@ -1,8 +1,8 @@
-import inquirer from "inquirer";
-import qr from "qr-image";
-import fs from "fs";
-import sillynames from "sillyname";
-import superheroes from "superheroes";
+import inquirer from "inquirer"; // Importerar inquirer
+import qr from "qr-image"; // Importerar qr-image
+import fs from "fs"; // Importerar fs
+import sillynames from "sillyname"; // Importerar sillyname
+import superheroes from "superheroes"; // Importerar superheroes
 
 // UPPGIFT 1 (hälsning)
 console.log("Hello World");
@@ -19,18 +19,22 @@ console.log("My silly name is " + sillynames() + "!");
 // UPPGIFT 3 Fortsättning (superheroes)
 console.log("My superhero name is " + superheroes.random() + "!");
 
-// UPPGIFT 4 (inquirer)
 
 
+// UPPGIFT 4 (inquirer & qr-image för att skapa en QR-kod från en URL)
 function randomChars(length) {
+    // För att skapa en unik filnamn för svg-filen
     let result = "";
     let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    // Loopar efter length
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result;
 }
 
+// Frågar efter en URL
 inquirer.prompt([
     {
         type: "input",
@@ -38,12 +42,16 @@ inquirer.prompt([
         name: "url"
     }
 ])
+    // Skapa en QR-kod från URLen som togs in
     .then((Answers) => {
-        console.log(Answers);
+        // SVG format
         let qr_svg = qr.image(Answers['url'], { type: "svg" });
-        qr_svg.pipe(fs.createWriteStream(`${randomChars(5)}.svg`));
-        let svg_string = qr.imageSync("I love QR!", { type: "svg" });
+        // Sparar ner svg-filen på disk
+        let tmpFileName = `${randomChars(5)}.svg`;
+        qr_svg.pipe(fs.createWriteStream(`${tmpFileName}.svg`));
+        console.log("QR code saved as svg as: " + tmpFileName + ".svg");
     })
+    // Error hantering
     .catch((error) => {
         if (error.isTtyError) {
             console.log("Prompt couldn't be rendered in the current environment");
