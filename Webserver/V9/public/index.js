02646337b49f6459c012bcf101ea78933fbb8954
William <william.jsson@hotmail.com>
2024-03-08 14:45:18 +0100

    womp womp

--- Webserver/V9/public/index.js
+++ Webserver/V9/public/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
+const fs = require("fs");
+const bodyParser = require('body-parser');
 
 let portNumber = 3000;
 let app = express();
 
+// Use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(__dirname, + "/public"));
+
+// Create logstream
+var accessLogStream = fs.createWriteStream(__dirname + 'access.log', { flags: 'a' })
+
+app.use(morgan(
+    "combined",
+    {stream: accessLogStream}
+));
 
 // Upon any get, log ip
 app.use((req, res, next) => {
@@ -30,6 +44,17 @@ app.get('/', (req, res) => {
 app.get('/womp', (req, res) => {
     res.sendFile(__dirname + '/womp.html');
     console.log('Connection: ' + req.ip + ' moved to womp page...');
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.usr;
+    const password = req.body.pass;
+
+    if(username === "william" && password === "Vincent"){
+        res.redirect("/wooo.html")
+    }else{
+        res.redirect("/?error=wrong-credentials");
+    }
 });
 
 // 404 page
