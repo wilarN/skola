1d726a407632b202421f3d2397e9ae0cedc8db34
William <william.jsson@hotmail.com>
2024-03-13 11:24:17 +0100

    Continue medium02

---
+++ Prog2/Grafik/0001/GFÖ/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GFÖ
+{
+    public partial class Form1 : Form
+    {
+        private Circle small01; // red
+        private Circle small02Divider; // black
+        private Circle small03Divider; // black
+        private Circle medium01; // greenyellow
+        private Circle smallSmall01; // smallest circle center
+        private Circle smallSmall01Divider; // behind the smallest center dot
+        private Circle smallSmall01Yellow;
+        private Circle smallSmall01YellowDivider;
+        private Circle medium02;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            small01 = new Circle(300, 300, 50);
+            small02Divider = new Circle(300, 300, 53);
+            medium01 = new Circle(300, 300, 100);
+            small03Divider = new Circle(300, 300, 103);
+            smallSmall01 = new Circle(300, 300, 10);
+            smallSmall01Divider = new Circle(300, 300, 13);
+            smallSmall01Yellow = new Circle(300, 300, 20);
+            smallSmall01YellowDivider = new Circle(300, 300, 23);
+            // Medium 02 continue
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            Brush fillBrushGY = new SolidBrush(Color.GreenYellow);
+            Brush fillBrushRed = new SolidBrush(Color.Red);
+            Brush fillBrushBlack = new SolidBrush(Color.Black);
+            Brush FillBrushYellow = new SolidBrush(Color.Yellow);
+
+
+            medium01.renderWithOutline(fillBrushGY, fillBrushBlack, e, small03Divider);
+
+            small01.renderWithOutline(fillBrushRed, fillBrushBlack, e, small02Divider);
+
+            smallSmall01Yellow.renderWithOutline(FillBrushYellow, fillBrushBlack, e, smallSmall01YellowDivider);
+
+            smallSmall01.renderWithOutline(fillBrushRed, fillBrushBlack, e, smallSmall01Divider);
+
+
+            // Cleanup
+            fillBrushGY.Dispose();
+            fillBrushRed.Dispose();
+            fillBrushBlack.Dispose();
+            FillBrushYellow.Dispose();
+        }
+
+    }
+}
+
---
+++ Prog2/Grafik/0001/GFÖ/Target.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GFÖ
+{
+    public class Circle
+    {
+        public double x;
+        public double y;
+        public double radius; 
+        public Circle(float x_param, float y_param, float radius_param)
+        {
+            x = x_param;
+            y = y_param;
+            radius = radius_param;
+        }
+
+        public void renderWithOutline(Brush selectedBrush, Brush selectedBrushOutline, PaintEventArgs e, Circle outline)
+        {
+            // Outline
+            e.Graphics.FillEllipse(selectedBrushOutline,
+                (float)(outline.x - outline.radius), (float)(outline.y - outline.radius),
+                (float)(outline.radius * 2), (float)(outline.radius * 2));
+
+            // Bigger
+            e.Graphics.FillEllipse(selectedBrush,
+                (float)(x - radius), (float)(y - radius),
+                (float)(radius * 2), (float)(radius * 2));
+
+        }
+    }
+}
