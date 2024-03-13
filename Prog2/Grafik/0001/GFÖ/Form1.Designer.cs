c81f06ab58994023d403f865f47f39bd413d6fe1
William <william.jsson@hotmail.com>
2024-03-13 12:26:56 +0100

    61:Formcs

--- Prog2/Grafik/0001/GFÖ/Form1.Designer.cs
+++ Prog2/Grafik/0001/GFÖ/Form1.Designer.cs
@@ -29,20 +29,45 @@ namespace GFÖ
         /// </summary>
         private void InitializeComponent()
         {
+            this.tbxRadius = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // tbxRadius
+            // 
+            this.tbxRadius.Location = new System.Drawing.Point(265, 47);
+            this.tbxRadius.Name = "tbxRadius";
+            this.tbxRadius.Size = new System.Drawing.Size(114, 20);
+            this.tbxRadius.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Update Radius Modifier";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxRadius);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbxRadius;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Prog2/Grafik/0001/GFÖ/Form1.cs
+++ Prog2/Grafik/0001/GFÖ/Form1.cs
@@ -15,47 +15,76 @@ namespace GFÖ
         private Circle smallSmall01Yellow;
         private Circle smallSmall01YellowDivider;
         private Circle medium02;
+        private Circle medium02Divider;
+        private double widthModifier;
+
+        private Brush fillBrushGY;
+        private Brush fillBrushRed;
+        private Brush fillBrushBlack;
+        private Brush fillBrushYellow;
+        private Brush fillBrushOrange;
 
         public Form1()
         {
             InitializeComponent();
 
-            small01 = new Circle(300, 300, 50);
-            small02Divider = new Circle(300, 300, 53);
-            medium01 = new Circle(300, 300, 100);
-            small03Divider = new Circle(300, 300, 103);
-            smallSmall01 = new Circle(300, 300, 10);
-            smallSmall01Divider = new Circle(300, 300, 13);
-            smallSmall01Yellow = new Circle(300, 300, 20);
-            smallSmall01YellowDivider = new Circle(300, 300, 23);
-            // Medium 02 continue
+            float xPlacement = this.Size.Width / 2;
+            float yPlacement = this.Size.Height / 2;
+            double widthModifier = 1;
+
+            small01 = new Circle(xPlacement, yPlacement, 50 * (float)widthModifier);
+            small02Divider = new Circle(xPlacement, yPlacement, 53 * (float)widthModifier);
+            medium01 = new Circle(xPlacement, yPlacement, 100 * (float)widthModifier);
+            small03Divider = new Circle(xPlacement, yPlacement, 103 * (float)widthModifier);
+            smallSmall01 = new Circle(xPlacement, yPlacement, 10 * (float)widthModifier);
+            smallSmall01Divider = new Circle(xPlacement, yPlacement, 13 * (float)widthModifier);
+            smallSmall01Yellow = new Circle(xPlacement, yPlacement, 20 * (float)widthModifier);
+            smallSmall01YellowDivider = new Circle(xPlacement, yPlacement, 23 * (float)widthModifier);
+            medium02 = new Circle(xPlacement, yPlacement, 75 * (float)widthModifier);
+            medium02Divider = new Circle(xPlacement, yPlacement, 78 * (float)widthModifier);
+
+            fillBrushGY = new SolidBrush(Color.GreenYellow);
+            fillBrushRed = new SolidBrush(Color.Red);
+            fillBrushBlack = new SolidBrush(Color.Black);
+            fillBrushYellow = new SolidBrush(Color.Yellow);
+            fillBrushOrange = new SolidBrush(Color.Orange);
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
+            fillBrushGY = new SolidBrush(Color.GreenYellow);
+            fillBrushRed = new SolidBrush(Color.Red);
+            fillBrushBlack = new SolidBrush(Color.Black);
+            fillBrushYellow = new SolidBrush(Color.Yellow);
+            fillBrushOrange = new SolidBrush(Color.Orange);
 
-            Brush fillBrushGY = new SolidBrush(Color.GreenYellow);
-            Brush fillBrushRed = new SolidBrush(Color.Red);
-            Brush fillBrushBlack = new SolidBrush(Color.Black);
-            Brush FillBrushYellow = new SolidBrush(Color.Yellow);
-
+            small01.radius = small01.radius * (float)widthModifier;
 
             medium01.renderWithOutline(fillBrushGY, fillBrushBlack, e, small03Divider);
+            medium02.renderWithOutline(fillBrushOrange, fillBrushBlack, e, medium02Divider);
 
             small01.renderWithOutline(fillBrushRed, fillBrushBlack, e, small02Divider);
 
-            smallSmall01Yellow.renderWithOutline(FillBrushYellow, fillBrushBlack, e, smallSmall01YellowDivider);
+
+            smallSmall01Yellow.renderWithOutline(fillBrushYellow, fillBrushBlack, e, smallSmall01YellowDivider);
 
             smallSmall01.renderWithOutline(fillBrushRed, fillBrushBlack, e, smallSmall01Divider);
 
-
-            // Cleanup
             fillBrushGY.Dispose();
             fillBrushRed.Dispose();
             fillBrushBlack.Dispose();
-            FillBrushYellow.Dispose();
+            fillBrushYellow.Dispose();
+            fillBrushOrange.Dispose();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(double.TryParse(tbxRadius.Text, out double newValue))
+            {
+                widthModifier = Math.Abs(newValue);
+                Invalidate();
+            }
+        }
     }
 }
 
