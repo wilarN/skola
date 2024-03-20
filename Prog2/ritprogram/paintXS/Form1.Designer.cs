a4fb69064b3dbea806f7b9b3eb4ef32121046167
William <william.jsson@hotmail.com>
2024-03-20 11:21:55 +0100

    Ritprogram

---
+++ Prog2/ritprogram/paintXS/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace paintXS
+{
+    partial class FRMPaintProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxPaintArea = new System.Windows.Forms.PictureBox();
+            this.lblDebugText = new System.Windows.Forms.Label();
+            this.tbSize = new System.Windows.Forms.TrackBar();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.pbColourSelection = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPaintArea)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbColourSelection)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxPaintArea
+            // 
+            this.pbxPaintArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxPaintArea.Location = new System.Drawing.Point(12, 129);
+            this.pbxPaintArea.Name = "pbxPaintArea";
+            this.pbxPaintArea.Size = new System.Drawing.Size(1197, 530);
+            this.pbxPaintArea.TabIndex = 0;
+            this.pbxPaintArea.TabStop = false;
+            this.pbxPaintArea.Paint += new System.Windows.Forms.PaintEventHandler(this.pbxPaintArea_Paint);
+            this.pbxPaintArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbxPaintArea_MouseDown);
+            this.pbxPaintArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbxPaintArea_MouseMove);
+            this.pbxPaintArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbxPaintArea_MouseUp);
+            // 
+            // lblDebugText
+            // 
+            this.lblDebugText.AutoSize = true;
+            this.lblDebugText.Location = new System.Drawing.Point(9, 9);
+            this.lblDebugText.Name = "lblDebugText";
+            this.lblDebugText.Size = new System.Drawing.Size(58, 13);
+            this.lblDebugText.TabIndex = 1;
+            this.lblDebugText.Text = "debugText";
+            // 
+            // tbSize
+            // 
+            this.tbSize.Location = new System.Drawing.Point(456, 23);
+            this.tbSize.Maximum = 100;
+            this.tbSize.Name = "tbSize";
+            this.tbSize.Size = new System.Drawing.Size(228, 45);
+            this.tbSize.SmallChange = 10;
+            this.tbSize.TabIndex = 2;
+            this.tbSize.Scroll += new System.EventHandler(this.tbSize_Scroll);
+            // 
+            // pbColourSelection
+            // 
+            this.pbColourSelection.BackColor = System.Drawing.Color.Black;
+            this.pbColourSelection.Location = new System.Drawing.Point(730, 32);
+            this.pbColourSelection.Name = "pbColourSelection";
+            this.pbColourSelection.Size = new System.Drawing.Size(185, 36);
+            this.pbColourSelection.TabIndex = 3;
+            this.pbColourSelection.TabStop = false;
+            this.pbColourSelection.Click += new System.EventHandler(this.pbColourSelection_Click);
+            // 
+            // FRMPaintProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1221, 671);
+            this.Controls.Add(this.pbColourSelection);
+            this.Controls.Add(this.tbSize);
+            this.Controls.Add(this.lblDebugText);
+            this.Controls.Add(this.pbxPaintArea);
+            this.Name = "FRMPaintProgram";
+            this.Text = "paintXS";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPaintArea)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbColourSelection)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxPaintArea;
+        private System.Windows.Forms.Label lblDebugText;
+        private System.Windows.Forms.TrackBar tbSize;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.PictureBox pbColourSelection;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Prog2/ritprogram/paintXS/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace paintXS
+{
+    public partial class FRMPaintProgram : Form
+    {
+        // Om användaren aktivt ritar
+        private bool isCurrentlyDrawing = false;
+
+        // Håller koll på tidigare point som ritats på
+        private Point previousPoint;
+
+        // En bitmap för att spara ritdata
+        private Bitmap drawingSurface = new Bitmap(1197, 530);
+
+        // Storlek på brush --> Default size 3
+        private int paintSize = 3;
+
+        // Färg --> Default Svart
+        private Color mainColor = Color.Black;
+
+        // Current tool
+        /*
+         CASES:
+        1. Normal pen
+        2. Square
+        3. Delete
+         */
+        private int currentTool = 2;
+
+        public FRMPaintProgram()
+        {
+            InitializeComponent();
+
+            // 
+            InitializeDrawingSurface();            
+        }
+
+        // Metod för att skapa ett ritområde genom att rensa det till vit färg.
+        private void InitializeDrawingSurface()
+        {
+            using (Graphics g = Graphics.FromImage(drawingSurface))
+            {
+                g.Clear(Color.White);
+            }
+        }
+
+
+        // Paintarea mouse down
+        private void pbxPaintArea_MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (currentTool)
+            {
+                case 1:
+                    isCurrentlyDrawing = true;                   // Användaren har börjat rita                
+                    previousPoint = e.Location;         // Sparar positionen där muspekaren befann sig när ritningen påbörjades i previousPoint 
+                    lblDebugText.Text = "KLICKAR";
+                    break;
+                case 2:
+                    previousPoint = e.Location;
+                    isCurrentlyDrawing = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void pbxPaintArea_MouseUp(object sender, MouseEventArgs e)
+        {
+            switch (currentTool)
+            {
+                case 1:
+                    isCurrentlyDrawing = false;                   // Användaren har börjat rita                
+                    lblDebugText.Text = "LYFTER";
+                    break;
+                case 2:
+                    using (Graphics g = Graphics.FromImage(drawingSurface))
+                    {
+                        // Square box
+                        SolidBrush solidBrush = new SolidBrush(mainColor);
+
+                        // Calculate the width and height of the square
+                        int width = e.X - previousPoint.X;
+                        int height = e.Y - previousPoint.Y;
+                        int size = Math.Min(width, height);
+
+                        g.FillRectangle(solidBrush, previousPoint.X, previousPoint.Y, size, size);
+
+                        solidBrush.Dispose();
+                        isCurrentlyDrawing = false;
+                        pbxPaintArea.Invalidate();
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+        }
+
+        private void pbxPaintArea_Paint(object sender, PaintEventArgs e)
+        {
+            // Rita inom paintArea
+            e.Graphics.DrawImage(drawingSurface, Point.Empty);
+        }
+
+        private void pbxPaintArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isCurrentlyDrawing)
+            {
+                using (Graphics g = Graphics.FromImage(drawingSurface))
+                {
+                    switch (currentTool)
+                    {
+                        case 1:
+                            // Normal pen
+                            // Skapa en penna
+                            Pen pen = new Pen(mainColor, paintSize);
+                            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+                            // Rita en linje från föregående musposition till nuvarande musposition med den svarta pennan
+                            g.DrawLine(pen, previousPoint, e.Location);
+                            pen.Dispose();
+
+                            previousPoint = e.Location;
+
+                            // Uppdatera PictureBox för att visa de ändringar som gjorts på ritområdet
+                            pbxPaintArea.Invalidate();
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            // Eraser
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                }
+
+            }
+        }
+
+        private void tbSize_Scroll(object sender, EventArgs e)
+        {
+            paintSize = tbSize.Value;
+        }
+
+        private void pbColourSelection_Click(object sender, EventArgs e)
+        {
+            colorDialog.ShowHelp = true;
+            colorDialog.AllowFullOpen = false;
+            colorDialog.Color = pbColourSelection.ForeColor;
+
+
+            if(colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                pbColourSelection.ForeColor = colorDialog.Color;
+                pbColourSelection.BackColor = colorDialog.Color;
+                mainColor = colorDialog.Color;
+            }
+        }
+    }
+}
