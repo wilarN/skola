e0aeebb30c600481cde92bf0d6711decad301ce5
William <william.jsson@hotmail.com>
2024-05-23 12:39:19 +0200

    Update

---
+++ polymorphism/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace poly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClasses.animal mainAnimal = new myClasses.animal();
+            mainAnimal.sound();
+
+            myClasses.pig mainPig = new myClasses.pig();
+            mainPig.sound();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ polymorphism/car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace myClasses
+{
+    class animal
+    {
+        public virtual void sound()
+        {
+            Console.WriteLine("Generic animal sound.");
+        }
+    }
+
+    class pig : animal
+    {
+        public override void sound()
+        {
+            Console.WriteLine("Oink oink.");
+        }
+    }
+}
